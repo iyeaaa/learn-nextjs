b08e57d855c34024e3044e6a64312e2a36db2d57
이예인 <iyeaaa@o.cnu.ac.kr>
2025-08-27 19:48:13 +0900

    last

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -6,17 +6,19 @@ import MovieInfo from "../../../../components/movie-info";
 import { getMovie } from "../../../../components/movie-info";
 
 interface MovieDetailProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
-export async function generateMetadata({ params: { id } }: MovieDetailProps) {
+export async function generateMetadata({ params }: MovieDetailProps) {
+  const { id } = await params;
   const movie = await getMovie(id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetail({ params: { id } }: MovieDetailProps) {
+export default async function MovieDetail({ params }: MovieDetailProps) {
+  const { id } = await params;
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
